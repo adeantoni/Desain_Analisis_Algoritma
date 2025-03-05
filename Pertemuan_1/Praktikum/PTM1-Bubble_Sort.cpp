4ea0c241e8813fda1c3ee01ea65bc7c991456602
adeantoni <242310010@student.ibik.ac.id>
2025-03-05 15:23:29 +0700

    first commit

---
+++ Pertemuan_1/Praktikum/PTM1-Bubble_Sort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int arr[], int n)
+{
+	int i, j, tmp;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (arr[j] > arr[j+1])
+			{		
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}	
+	}
+}
+
+int main()
+{
+	int array[100], n, i;
+	
+	cout << "Masukkan jumlah elemen: ";
+	cin >> n;
+	
+	cout << "Masukkan nilai:\n";
+	for (i = 0; i < n; i++)
+	{
+		cin >> array[i];
+	}
+	
+	cout << "Array sebelum di sort:\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << array[i] << ' ';
+	}
+	cout << endl;
+	
+	bubble_sort(array, n);
+	
+	cout << "Hasil setelah di Bubble Sort:\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << array[i] << ' ';
+	}
+}
